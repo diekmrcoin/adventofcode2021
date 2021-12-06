4800cc0686334e8ae9511fa1d95adf2f21ec9e79
Diego Maroto <diego.diemg@gmail.com>
2021-12-06 11:31:48 +0100

    Refactor day 1 to easier copypaste for new days. Starting day 3.

---
+++ day03/README.md
@@ -0,0 +1,2 @@
+- [Day 3 link](https://adventofcode.com/2021/day/3)
+- [Input link](https://adventofcode.com/2021/day/3/input)
--- day03/main.js
+++ day03/main.js
@@ -3,19 +3,6 @@ console.log(main());
 
 function main() {
   const nums = readInput().map(Number);
-  let lastMeasure;
-  let increase = 0;
-  for (const num of nums) {
-    if (lastMeasure === undefined) {
-      lastMeasure = num;
-      continue;
-    }
-    if (num > lastMeasure) {
-      increase++;
-    }
-    lastMeasure = num;
-  }
-  return increase;
 }
 
 /**

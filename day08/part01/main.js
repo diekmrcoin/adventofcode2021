1e9a5bd48ae3fe9d34f41bb40b22537d1f9a1f37
Diego Maroto <diego.diemg@gmail.com>
2021-12-08 09:27:36 +0100

    Day 8 part 1 completed

--- day08/part01/main.js
+++ day08/part01/main.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const { expect } = require('chai');
 const result = main();
-const expected = 0;
+const expected = 476;
 expect(result).to.be.eq(expected);
-console.log("Day 08, Part 01:", result);
+console.log('Day 08, Part 01:', result);
 
 function main() {
-  const input = readInput();
+  const input = readInput().filter(row => !!row).map(row => row.split(' | '));
+  let total = 0;
+  for (const row of input) {
+    const numbers = row[1].split(' ');
+    for (const number of numbers) {
+      if (validNumber(number)) total++;
+    }
+  }
+  return total;
 }
 
 /**
@@ -14,4 +22,9 @@ function main() {
  */
 function readInput() {
   return require('fs').readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\n');
+}
+
+function validNumber(number) {
+  const lengths = [2, 4, 3, 7];
+  return lengths.some(length => number.length === length);
 }
